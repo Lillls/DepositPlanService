b960699c19eb3cb90597b71e597654601b47c2da
lixxy <lixiaoyu3@cmcm.com>
2019-03-27 11:57:01 +0800

    增加service层..

--- src/main/java/com/lixxy/depositplan/service/PlanService.java
+++ src/main/java/com/lixxy/depositplan/service/PlanService.java
@@ -4,9 +4,11 @@ import com.lixxy.depositplan.dao.PlanMapper;
 import com.lixxy.depositplan.model.PlanBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PlanService {
     @Autowired
     PlanMapper planMapper;
